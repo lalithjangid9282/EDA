6febc504b896e98067cda46e8d01b58809412bdc
Lalith Kumar <lalithjangid6970@gmail.com>
2025-08-21 23:13:59 +0530

    Initial Commit

--- Spotify_Song_Analysis/Spotify_app.py
+++ Spotify_Song_Analysis/Spotify_app.py
@@ -15,7 +15,7 @@ def load_data():
     Loads the Spotify dataset and performs necessary data cleaning.
     """
     try:
-        df = pd.read_csv("SpotifyFeatures.csv")
+        df = pd.read_csv("Spotify.csv")
     except FileNotFoundError:
         st.error("Error: 'SpotifyFeatures.csv' not found. Please ensure the file is in the same directory.")
         return None
